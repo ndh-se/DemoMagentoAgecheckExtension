a30f488d6e111430444b0a6751a1e602b20b3bfe
ndh-se <ndh.se.46@gmail.com>
2016-12-09 20:20:48 +0700

    Add files via upload

---
+++ Demo/Agecheck/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            ageCheck: 'Demo_Agecheck/lib/age-check/jquery.agecheck'
+        }
+    },
+	shim: {
+		"3-rd-party-plugin": ["jquery"]
+	}
+};
